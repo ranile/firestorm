45949045126941a6b258f86e08043b6d3183a823
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-05-08 15:02:47 +0500

    Max room name length

--- src/lib/components/SideNav/CreateRoomModal.svelte
+++ src/lib/components/SideNav/CreateRoomModal.svelte
@@ -1,5 +1,5 @@
-<script lang="ts">
-    import { Button, Modal, Label, Input } from 'flowbite-svelte';
+<script lang='ts'>
+    import { Button, Modal, Label, Input, Helper } from 'flowbite-svelte';
     import { createRoomModalState } from './store';
     import { createRoom } from '../../db/rooms';
     import { supabase } from '../../supabase';
@@ -13,18 +13,22 @@
     };
 </script>
 
-<Modal bind:open={$createRoomModalState} size="xs" class="w-full">
-    <form class="flex flex-col space-y-6" action="#" on:submit|preventDefault={onCreateClick}>
-        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Create a room</h3>
-        <Label class="space-y-2">
+<Modal bind:open={$createRoomModalState} size='xs' class='w-full'>
+    <form class='flex flex-col space-y-6' action='#' on:submit|preventDefault={onCreateClick}>
+        <h3 class='mb-4 text-xl font-medium text-gray-900 dark:text-white'>Create a room</h3>
+        <Label class='space-y-2'>
             <span>Name</span>
             <Input
                 bind:value={name}
-                name="name"
-                placeholder="What do you want to call your room?"
+                name='name'
+                placeholder='What do you want to call your room?'
+                max={64}
                 required
             />
+            <Helper class='text-sm mt-2'>
+                <span class='text-gray-500 dark:text-gray-400'>Max 64 characters</span>
+            </Helper>
         </Label>
-        <Button type="submit" class="w-full1">Create</Button>
+        <Button type='submit' class='w-full1'>Create</Button>
     </form>
 </Modal>
