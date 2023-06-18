584a076d1c236442691cc565fc0292a2cafc2043
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-06-18 21:27:44 +0500

    use ProfileUpdate component in onboarding

--- src/routes/(auth)/auth/onboarding/+page.svelte
+++ src/routes/(auth)/auth/onboarding/+page.svelte
@@ -4,17 +4,14 @@
     import type { PageData } from './$types';
     import { goto } from '$app/navigation';
     import { updateProfile } from '$lib/db/users';
+    import ProfileUpdate from '$lib/components/ProfileUpdate.svelte';
 
     export let data: PageData;
 
     let username = '';
-    let files: FileList | undefined = undefined;
-    let fileInput: HTMLInputElement;
-    const imageUploadClicked = () => {
-        fileInput.click();
-    };
     let errorMessage = '';
     let inputErrorMessage = '';
+    let avatarFile: File | undefined = undefined;
 
 
     const getStartedClick = async () => {
@@ -24,7 +21,7 @@
         }
         const userId = data.session!.user.id;
         try {
-            await updateProfile(data.supabase, userId, username, files?.item(0));
+            await updateProfile(data.supabase, userId, username, avatarFile);
             await goto('/');
         } catch (e) {
             if (e instanceof Error) {
@@ -37,30 +34,7 @@
 <div class="flex w-full md:h-full">
     <main class="flex flex-col mx-auto md:my-auto min-w-auth-card gap-4">
         <h1 class="self-center font-bold text-xl p-5 uppercase">Welcome</h1>
-        <div class="flex gap-4 w-full">
-            <section class="flex items-center">
-                <input
-                    type="file"
-                    class="hidden"
-                    bind:files
-                    bind:this={fileInput}
-                    accept="image/*"
-                />
-                <Button btnClass="relative" on:click={imageUploadClicked}>
-                    <Avatar class="h-14 w-14" />
-                    <PlusCircle class="absolute -bottom-1 -right-0 h-6 w-6" />
-                </Button>
-            </section>
-            <section class="flex flex-col w-full">
-                <h2 class="py-4">Choose a username</h2>
-                <Input bind:value={username} placeholder="Username" required />
-                {#if inputErrorMessage !== ''}
-                    <Helper class="mt-2" color="red">
-                        <span class="font-medium">{inputErrorMessage}</span>
-                    </Helper>
-                {/if}
-            </section>
-        </div>
+        <ProfileUpdate bind:username bind:avatarFile />
         <Button on:click={getStartedClick}>Get Started</Button>
         {#if errorMessage !== ''}
             <Helper class="mt-2" color="red">
