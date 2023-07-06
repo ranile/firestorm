55b22849dc704658cd1d82c2d4bc2a8ea314ff3f
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-07-06 22:36:39 +0500

    deployment + CI

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             runtime: 'nodejs18.x',
-            regions: 'all',
-            split: true
+            split: false
         })
     }
 };
